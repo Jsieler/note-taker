fc833c30ddf1cd34b204d45661126c9417161925
Jsieler <joshuasieler@Joshs-Air.home>
2021-07-11 12:49:45 -0600

    fixed express static

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+
 const api = require('./routes/apiroutes')
 const html = require("./routes/htmlroutes")
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use((req, res) => {
-    res.status(404).end();
-});
-
-
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+
 app.use("/", html)
 app.use("/api", api)
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
